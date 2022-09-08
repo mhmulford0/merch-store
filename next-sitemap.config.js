a0e932bbac9d4655c570374f459197759c209857
Matthew Mulford <mhmulford0@gmail.com>
2022-09-08 15:40:35 -0500

    chore: post fork cleanup

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,7 @@
 module.exports = {
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: 'https://merch.developerdao.com',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
